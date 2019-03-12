ff2041113696ef9a15ae4558cc87b58f940ef9c5
admin <admin@server.local>
2019-03-12 16:45:19 +0100

    Initial Commit

---
+++ OrthanServerAdmin/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OrthanServerAdmin
+//
+//  Created by admin on 12/03/2019.
+//  Copyright © 2019 Innovaorto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
